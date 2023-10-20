6be2366d1d08d07a29620c68a0e1479345e41b45
mac <josiah.adesanya@gmail.com>
2023-10-20 03:43:15 +0100

    COnditional Statement and Ternary Operator

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        int n = 3;
+        boolean condition = n%2==0;
+        String x = condition?"this is even":"this is odd";
+        System.out.println(x);
+       /*if (n%2==0){
+            System.out.println("this is even");
+        }else{
+            System.out.println("this is odd");
+
+
+        }*/
+
+
+    }
+}
